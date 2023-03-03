4d785cee62d3dcac2cc5a9089e2e403b83efb389
Daniel Ochuba <danielochuba78@gmail.com>
2023-03-03 17:30:24 +0100

    initial To- DO structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+const toDoTasks = [{
+  description: 'Eat food',
+  completed: false,
+  index: 0,
+}, {
+  description: 'sleep',
+  completed: false,
+  index: 1,
+}, {
+  description: 'Watch videos',
+  completed: false,
+  index: 2,
+}];
+
+const listContainer = document.querySelector('.to-do');
+
+toDoTasks.forEach((task) => {
+  listContainer.innerHTML += `
+  <li class="listItem"><input type="checkbox" class="check" id="${task.index}">${task.description} <i class="fa-solid icon fa-ellipsis-vertical"></i></li>`;
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,38 @@
-
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- module.exports = {
-   entry: {
-     index: './src/index.js',
-   },
-   devServer: {
+module.exports = {
+  entry: {
+    index: './src/index.js',
+  },
+  devServer: {
     static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
       template: './src/index.html',
     }),
   ],
-   output: {
-     filename: '[name].bundle.js',
-     path: path.resolve(__dirname, 'dist'),
-   },
-   module: {
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        use: [
+          'file-loader',
+        ],
       },
     ],
   },
   optimization: {
     runtimeChunk: 'single',
   },
- };
+};
