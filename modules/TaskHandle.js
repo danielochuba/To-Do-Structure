9f107537379ecb66d745c80a67d79856f1d852c2
Daniel Ochuba <danielochuba78@gmail.com>
2023-03-11 07:54:37 +0100

    make changes

--- modules/TaskHandle.js
+++ modules/TaskHandle.js
@@ -1,23 +1,5 @@
 const todoList = document.querySelector('.todo-list');
 
-//  function updateTodos(newTodos) {
-//   const updatedTodos = [];
-//   for (let i = 0; i < newTodos.length; i += 1) {
-//     updatedTodos.push({ ...newTodos[i], id: i + 1 });
-//   }
-
-//   localStorage.setItem('todos', JSON.stringify(updatedTodos));
-
-//   const description = updatedTodos.map((todo) => `
-//   <li class="card todo-list-item ${todo.completed ? 'completed' : ''}" data-id="${todo.id}">
-//     <input type="checkbox" ${todo.completed ? 'checked' : ''} class="checkbox"/>
-//     <input type="text" value="${todo.description}" class="inputtext" id="${todo.id}"/>
-//     <button type="button">🗑</button>
-//   </li>
-// `).join('');
-
-//   todoList.innerHTML = description;
-// }
 /**
  * Updates the todos list with new items.
  *
